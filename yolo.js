3ca3cfa268b75b7bd7e23c691a48bfec41535274
Daniel Moreh <ctrlplus1daniel@gmail.com>
2014-08-02 00:30:40 -0700

    YOLO

--- yolo.js
+++ yolo.js
@@ -1,1 +1,12 @@
-console.log("hello")
+var submitButtons = document.querySelectorAll('[type=submit]');
+var YOLO_STRING = 'YOLO';
+
+for (var i = 0; i < submitButtons.length; ++i) {
+  var item = submitButtons[i];
+  if (item.tagName === 'INPUT') {
+    item.value = YOLO_STRING;
+  } else {
+    item.innerHTML = YOLO_STRING;
+  }
+}
+
